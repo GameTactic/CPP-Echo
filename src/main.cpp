a8a35fd6c8fc031b5ae169c27eb6bb6c6e417d4e
Niko Granö <niko@ironlions.fi>
2019-12-24 00:59:56 +0200

    Implement Debug feature.

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,12 @@ public:
             std::cout << "|     Copyright 2019 Niko Granö <niko@ironlions.fi>    |\n";
             std::cout << "|                 Licensed under GPLv3.                |\n";
             std::cout << "|                 https://gametactic.eu                |\n";
-            std::cout << "--------------------------------------------------------\n\n";
+            std::cout << "--------------------------------------------------------\n";
+            if (debug == true) {
+                std::cout << "[NOTICE]: Debug output enabled!!! \n\n";
+            } else {
+                std::cout << "\n";
+            }
     }
     void run()
     {
@@ -106,7 +111,11 @@ public:
         } catch (json::exception&) {
             output["success"] = false;
             output["error"] = "Invalid JSON Syntax.";
-            ptr->send(output.dump());
+            std::string stringOutput = output.dump();
+            std::stringstream debugOutput;
+            debugOutput << "Invalid json given. Sending: " << stringOutput << ".\n";
+            logDebug(debugOutput.str());
+            ptr->send(stringOutput);
 
             return;
         }
@@ -116,7 +125,11 @@ public:
             std::string room = input.at("join_room");
             ptr->room = room;
             output["success"] = true;
-            ptr->send(output.dump());
+            std::string stringOutput = output.dump();
+            std::stringstream debugOutput;
+            debugOutput << "Joining room " << room << ". Sending: " << stringOutput << ".\n";
+            logDebug(debugOutput.str());
+            ptr->send(stringOutput);
 
             return;
         } catch (json::exception&) {
@@ -126,7 +139,11 @@ public:
         if (ptr->room.empty()) {
             output["success"] = false;
             output["error"] = "No room selected.";
-            ptr->send(output.dump());
+            std::string stringOutput = output.dump();
+            std::stringstream debugOutput;
+            debugOutput << "No room selected. Sending: " << stringOutput << ".\n";
+            logDebug(debugOutput.str());
+            ptr->send(stringOutput);
 
             return;
         }
@@ -134,6 +151,9 @@ public:
         // Build message as defined in https://github.com/GameTactic/CPP-Echo/issues/20
         output["payload"] = input;
         std::string stringOutput = output.dump();
+        std::stringstream debugOutput;
+        debugOutput << "Sending " << stringOutput << " to room " << ptr->room << ".\n";
+        logDebug(debugOutput.str());
 
         try {
             for (auto c : _connections) {
